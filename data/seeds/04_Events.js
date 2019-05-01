8f49938be095f960d799e6f1368691dcfefbb9ed
Kseniya Platonava <ksu2008@gmail.com>
2019-04-30 23:56:02 -0400

    Fix PUT, Add Evens/id

--- data/seeds/04_Events.js
+++ data/seeds/04_Events.js
@@ -7,6 +7,11 @@ exports.seed = function(knex, Promise) {
       return knex('events').insert([
         { event_name: 'Lambda Conference',
          event_date: '1.9.2019'},
+         { event_name: 'IBM Conference',
+         event_date: '1.9.2019'},
+         { event_name: 'Oracle Conference',
+         event_date: '1.9.2019'}
+
     
       ]);
     });
